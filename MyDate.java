fe72b8d5817f59f13c874887f64d4c5d2f534668
GeorgeN <91430969+Gevoooo@users.noreply.github.com>
2022-04-18 14:44:06 -0400

    Update MyDate.java

--- MyDate.java
+++ MyDate.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.georgenakashyan.lab1.homework3;
 
 import com.google.gson.annotations.SerializedName;
 
 /**
  *
- * @author George Nakashyan
+ * @author Gevoooo
  */
 public class MyDate {
     
